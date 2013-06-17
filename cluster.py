055932f1cd190aea32e230fe81f578ef246a4848
Mike Heilman <mheilman@ets.org>
2013-06-17 18:42:52 -0400

    added comments, logging

--- cluster.py
+++ cluster.py
@@ -1,18 +1,49 @@
 #!/bin/env python3
 
-import json
+'''
+A little module for creating hierarchical word clusters.
+This is based on the following paper.
+
+Peter F. Brown; Peter V. deSouza; Robert L. Mercer; T. J. Watson; Vincent J. 
+Della Pietra; Jenifer C. Lai. 1992.  Class-Based n-gram Models of Natural 
+Language.  Computational Linguistics, Volume 18, Number 4.  
+http://acl.ldc.upenn.edu/J/J92/J92-4003.pdf
+
+While this code creates hierarchical clusters, it does not use the HMM-like 
+sequence model to do so (section 3).  Instead, it merges clusters similar to the
+technique described in section 4 of Brown et al. (1992), using pointwise mutual 
+information.  However, the formulation of PMI used here differs slightly: 
+each word/cluster is associated with a list of documents that it appears in 
+(documents can be full texts, sentences, tweets, etc.).  
+The score for merging two clusters c1 and c2 is proportional to the following:
+
+  log [ p(c1 and c2 in document) / p(c1 in document) / p(c2 in document) ]
+
+The probabilities are maximum likelihood estimates (e.g., number of documents
+with c1 divided by the number of documents).  Since the total number of 
+documents is constant, we just use the counts instead of relative frequencies.
+
+Also, see http://www.cs.columbia.edu/~cs4705/lectures/brown.pdf for a nice 
+overview.
+
+Another implementation of "Brown clustering":
+https://github.com/percyliang/brown-cluster
+
+Author: Michael Heilman
+
+'''
+
 import sys
 import argparse
 import glob
 import re
 import itertools
-#import math
+import logging
 from collections import defaultdict
 from time import gmtime, strftime
 import numpy
 
-def curtimestr():
-    return strftime("%Y-%m-%d %H:%M:%S", gmtime())
+logging.basicConfig(level=logging.INFO, format='%(asctime)s\t%(message)s')
 
 def document_generator(path):
     with open(path) as f:
@@ -29,26 +60,53 @@ def test_doc_gen():
 
 
 class DocumentLevelClusters(object):
+    '''
+    The initializer takes a document generator, which is simply an iterator 
+    over lists of tokens.  You can define this however you wish.
+    '''
     def __init__(self, doc_generator, batch_size=1000):
         self.batch_size = batch_size
+
+        # mapping from cluster IDs to cluster IDs, 
+        # to keep track of the hierarchy
         self.cluster_parents = {}
         self.cluster_counter = 0
-        self.index = defaultdict(set)  # word ID -> list of doc IDs
-        self.word_bitstrings = {}  # word/cluster labels to bitstrings (initialize to include all words as keys)
-        self.create_index(doc_generator)
-        self.cluster_bits = {}
-        self.log_counts = {}
+
+        # word ID -> list of doc IDs
+        self.index = defaultdict(set)
+
+        # word/cluster labels to bitstrings
+        # (initialize to include all words as keys)
+        self.word_bitstrings = {}
         
-        # initialize the dictionary of classes to consider merging and their log counts
+        # the bit to add when walking up the hierarchy 
+        # from a word to the top-level cluster
+        self.cluster_bits = {}
+
+        # cache of log counts for words/clusters
+        self.log_counts = {}
+
+        # create sets of documents that each word appears in
+        self.create_index(doc_generator)
+        
+        # find the most frequent words, score potential clusters for the most 
+        # frequent words.  Note that the count-based score is proportional to 
+        # the PMI since the probabilities are divided by a constant for the
+        # number of documents in the input.
         most_common_words = sorted(self.index.keys(), key=lambda x: -len(self.index[x]))
-        self.current_batch = most_common_words[:self.batch_size]
-        self.current_batch_scores = [(self.log_count_pair(c1, c2) - self.log_count(c1) - self.log_count(c2), (c1, c2)) for c1, c2 in itertools.combinations(self.current_batch, 2)]
+        self.current_batch = most_common_words[:(self.batch_size + 1)]
+        self.current_batch_scores = [(self.log_count_pair(c1, c2)
+                                      - self.log_count(c1) 
+                                      - self.log_count(c2), (c1, c2)) 
+                                     for c1, c2 in 
+                                     itertools.combinations(self.current_batch, 2)]
 
-        most_common_words = most_common_words[self.batch_size:]
+        # remove the first batch_size elements
+        most_common_words = most_common_words[(self.batch_size + 1):]
 
-        print('\n{} CLUSTERING'.format(curtimestr()), file=sys.stderr)
+        logging.info('CLUSTERING')
         while len(self.index) > 1:
-            # find the best to merge
+            # find the best pair of words/clusters to merge
             c1, c2 = self.find_best(self.current_batch_scores)
             
             # pick the new cluster ID and increment the cluster index counter
@@ -57,26 +115,37 @@ class DocumentLevelClusters(object):
             # merge the clusters in the index
             self.merge(c1, c2, new_cluster)
 
-            # remove the merged clusters from the batch, add the new one and the next most frequent word (if available)
+            # remove the merged clusters from the batch, add the new one
+            # and the next most frequent word (if available)
             self.update_batch(c1, c2, new_cluster, most_common_words)
 
-            print('{}\t{} AND {} WERE MERGED INTO {}. {} REMAIN.'.format(curtimestr(), c1, c2, new_cluster, len(self.index)), file=sys.stderr)
-
+            logging.info('{} AND {} WERE MERGED INTO {}. {} REMAIN.'
+                  .format(c1, c2, new_cluster, len(self.index)))
             self.cluster_counter += 1
 
+        # walk up the hierarchy from each word to create its bitstring
         self.create_bitstrings()
 
     def update_batch(self, c1, c2, new_cluster, most_common_words):
-        self.current_batch = [x for x in self.current_batch if x != c1 and x != c2]
-        self.current_batch_scores = [x for x in self.current_batch_scores if not (x[1][0] is c1 or x[1][1] is c1 or x[1][0] is c2 or x[1][1] != c2)]
-        # TODO might be able to make this list comprehension faster by using integer IDs
+        # remove the clusters that were merged (and the scored pairs for them)
+        self.current_batch = [x for x in self.current_batch 
+                              if x != c1 and x != c2]
+        self.current_batch_scores = [x for x in self.current_batch_scores 
+                                     if not (x[1][0] is c1 or x[1][1] is c1 
+                                             or x[1][0] is c2 or x[1][1] != c2)]
         
+        # find what to add to the current batch
         new_items = [new_cluster]
         if most_common_words:
             new_word = most_common_words.pop(0)
             new_items.append(new_word)
 
-        self.current_batch_scores.extend((self.log_count_pair(n1, n2) - self.log_count(n1) - self.log_count(n2), (n1, n2)) for n1, n2 in itertools.product(new_items, self.current_batch))
+        # add to the batch and score the new cluster pairs that result
+        self.current_batch_scores.extend((self.log_count_pair(n1, n2)
+                                          - self.log_count(n1) 
+                                          - self.log_count(n2), (n1, n2)) 
+                                          for n1, n2 in 
+                                          itertools.product(new_items, self.current_batch))
         self.current_batch.extend(new_items)
 
     def find_best(self, current_batch_scores):
@@ -89,7 +158,7 @@ class DocumentLevelClusters(object):
 
     def create_bitstrings(self):
         for w in self.word_bitstrings:
-            # walk up the tree until there is no parent cluster
+            # walk up the cluster hierarchy until there is no parent cluster
             cur_cluster = w
             bitstring = ""
             while cur_cluster in self.cluster_parents:
@@ -106,29 +175,13 @@ class DocumentLevelClusters(object):
     def log_count_pair(self, c1, c2):
         count = len(self.index[c1] & self.index[c2])
         if count == 0:
-            return float('-inf')
+            return float('-inf')  # log(0)
         return numpy.log(count)
-
-    def find_pair_to_merge(self, current_batch):
-        max_score = float('-inf')
-        argmax_score = None
-        
-        count_c1_c2 = 0
-        score = 0
-        for (c1, log_count_c1), (c2, log_count_c2) in itertools.combinations(current_batch.items(), 2):
-            count_c1_c2 = len(self.index[c1] & self.index[c2])
-            if count_c1_c2 == 0:
-                continue
-            score = numpy.log(count_c1_c2) - log_count_c1 - log_count_c2
-            if score > max_score:
-                max_score = score
-                argmax_score = (c1, c2)
-        return argmax_score
 
     def merge(self, c1, c2, new_id):
         self.cluster_parents[c1] = new_id
         self.cluster_parents[c2] = new_id
-        self.cluster_bits[c1] = '0'
+        self.cluster_bits[c1] = '0'  # assign the bits arbitrarily
         self.cluster_bits[c2] = '1'
 
         self.index[new_id] = self.index[c1] | self.index[c2]
@@ -143,19 +196,23 @@ class DocumentLevelClusters(object):
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Create hierarchical word clusters from a corpus follow Brown et al. (1992).')
-    parser.add_argument('input_path', help='input file, one document per line, with whitespace-separated tokens.')
+    parser = argparse.ArgumentParser(description='Create hierarchical word' +
+        ' clusters from a corpus, following Brown et al. (1992).')
+    parser.add_argument('input_path', help='input file, one document per' +
+        ' line, with whitespace-separated tokens.')
     parser.add_argument('output_path', help='output path')
     args = parser.parse_args()
 
     c = DocumentLevelClusters(args.input_path)
 
     with open(args.output_path, 'w') as f:
-        json.dump(c.word_bitstrings, f)
+        for key, val in c.word_bitstrings:
+            print("{}\t{}".format(key, val), file=f)
 
 if __name__ == '__main__':
     # main()
     c = DocumentLevelClusters(test_doc_gen())
 
-    with open('bitstrings.json', 'w') as f:
-        json.dump(c.word_bitstrings, f)
+    with open('bitstrings.txt', 'w') as f:
+        for key, val in c.word_bitstrings:
+            print("{}\t{}".format(key, val), file=f)
