b23a5e86e7fee7989eb3e3026f599ba60ca41be0
Mike Heilman <mheilman@ets.org>
2013-08-09 18:20:12 -0400

    converted ints to longs to support larger datasets

--- src/tanclustering/PMICluster.java
+++ src/tanclustering/PMICluster.java
@@ -35,11 +35,11 @@ public class PMICluster {
 	private Map<Integer, Integer> clusterBits = new HashMap<Integer, Integer>();
 
 	private List<String> words = new ArrayList<String>();
-	private Map<Integer, Integer> wordCounts = new HashMap<Integer, Integer>();
+	private Map<Integer, Long> wordCounts = new HashMap<Integer, Long>();
 	private Random rng = new Random(1234567890);
 
 	// clusterID -> {docID -> counts}
-	private Map<Integer, Map<Integer, Integer> > index = new HashMap<Integer, Map<Integer, Integer> >();
+	private Map<Integer, Map<Integer, Long> > index = new HashMap<Integer, Map<Integer, Long> >();
 
 	private Integer clusterCounter = 0;
 
@@ -65,12 +65,12 @@ public class PMICluster {
 				Integer c1 = tuple.v1;
 				Integer c2 = tuple.v2;
 
-				int paircount = 0;
+				long paircount = 0;
 
-				Map<Integer, Integer> index1 = index.get(c1);
-				Map<Integer, Integer> index2 = index.get(c2);
+				Map<Integer, Long> index1 = index.get(c1);
+				Map<Integer, Long> index2 = index.get(c2);
 				if(index1.size() > index2.size()){
-					Map<Integer, Integer> tmp = index1;
+					Map<Integer, Long> tmp = index1;
 					index1 = index2;
 					index2 = tmp;
 				}
@@ -85,6 +85,7 @@ public class PMICluster {
 					res.add(Double.NEGATIVE_INFINITY);  // log(0)
 				}else{
 					double score = Math.log(paircount) - Math.log(wordCounts.get(c1)) - Math.log(wordCounts.get(c2));
+					assert !Double.isNaN(score);
 					res.add(score);
 				}
 			}
@@ -114,10 +115,10 @@ public class PMICluster {
 		makeWordCounts(inputPath, maxVocabSize, minWordCount);
 
 		// make a copy of the list of words, as a queue for making new clusters
-		LinkedList<Integer> wordQueue = new LinkedList<Integer>();
+		LinkedList<Integer> wordStack = new LinkedList<Integer>();
 		for(String w: words){
 			Integer wordID = wordIDs.get(w);
-			wordQueue.add(wordID);
+			wordStack.add(wordID);
 			clusterCounter = wordID;
 		}
 		clusterCounter++;
@@ -129,8 +130,8 @@ public class PMICluster {
 		// score potential clusters, starting with the most frequent words.
 		// also, remove the batch from the queue
 		LOGGER.info("computing initial scores...");
-		for(int i = 0; i <= batchSize && wordQueue.size() > 0; i++){
-			currentBatch.add(wordQueue.pop());
+		for(int i = 0; i <= batchSize && wordStack.size() > 0; i++){
+			currentBatch.add(wordStack.pop());
 		}
 		makePairScores(allPairs(currentBatch));
 
@@ -146,9 +147,9 @@ public class PMICluster {
 
 			// remove the merged clusters from the batch, add the new one
 			// and the next most frequent word (if available)
-			updateBatch(c1, c2, wordQueue);
+			updateBatch(c1, c2, wordStack);
 
-			LOGGER.info(idToWord(c1) + " AND " + idToWord(c2) + " WERE MERGED INTO " + clusterCounter + ". " + (currentBatch.size() + wordQueue.size() - 1) + " REMAIN.");
+			LOGGER.info(idToWord(c1) + " AND " + idToWord(c2) + " WERE MERGED INTO " + clusterCounter + ". CURRENTBATCH: " + currentBatch.size() + " WORDSTACK:" + wordStack.size());
 
 			clusterCounter++;
 		}
@@ -187,14 +188,14 @@ public class PMICluster {
 				Integer wordID = wordIDs.get(tok);
 
 				if(!index.containsKey(wordID)){
-					index.put(wordID, new HashMap<Integer, Integer>());
+					index.put(wordID, new HashMap<Integer, Long>());
 				}
 
 				if(!index.get(wordID).containsKey(docID)){
-					index.get(wordID).put(docID, 0);
+					index.get(wordID).put(docID, 0L);
 				}
-				Integer tmpVal = index.get(wordID).get(docID);
-				index.get(wordID).put(docID, tmpVal + 1);
+				Long tmpVal = index.get(wordID).get(docID);
+				index.get(wordID).put(docID, tmpVal + 1L);
 			}
 			docID = Integer.valueOf(docID + 1);
 		}
@@ -234,8 +235,11 @@ public class PMICluster {
 			}
 		}
 
-		res.addAll(allPairs(newItems));
+		if(newItems.size() > 1){
+			res.addAll(allPairs(newItems));
+		}
 
+		// LOGGER.info("newPairs res: " + res.size() + " currentBatch.size():" + currentBatch.size());
 		return res;
 	}
 
@@ -245,6 +249,7 @@ public class PMICluster {
 
 		int n = pairList.size();
 
+		// LOGGER.info("makePairScores pairList.size(): " + pairList.size());
 		if(n == 0){
 			return;
 		}
@@ -305,23 +310,31 @@ public class PMICluster {
 		Integer c2;
 		double score;
 
+		// int n = 0;
+		// int m = 0;
 		for(Entry<Integer, Map<Integer, Double> > entry1: currentBatchScores.entrySet()){
 			c1 = entry1.getKey();
 			for(Entry<Integer, Double> entry2: entry1.getValue().entrySet()){
 				c2 = entry2.getKey();
 				score = entry2.getValue();
-				if(score > bestScore){
-					argmaxListC1.clear();
-					argmaxListC2.clear();
+				if(score >= bestScore){
+					if(score > bestScore){
+						argmaxListC1.clear();
+						argmaxListC2.clear();
+						bestScore = score;
+					}
 					argmaxListC1.add(c1);
 					argmaxListC2.add(c2);
-					bestScore = score;
-				}else if(score == bestScore){
-					argmaxListC1.add(c1);
-					argmaxListC2.add(c2);
+					// m++;
 				}
+				// n++;
 			}
 		}
+
+		// LOGGER.info("findBest: m = " + m + " n = " + n);
+		// if(n < 10){
+		// 	LOGGER.info(currentBatchScores.toString());
+		// }
 
 		// break ties randomly
 		int r = rng.nextInt(argmaxListC1.size());
@@ -341,13 +354,13 @@ public class PMICluster {
 		// initialize the document counts of the new cluster with the counts
 		// for one of the two child clusters.  then, add the counts from the
 		// other child cluster
-		Map<Integer, Integer> indexNew = index.get(c1);
+		Map<Integer, Long> indexNew = index.get(c1);
 		index.put(clusterCounter, indexNew);
 		for(Integer docID: index.get(c2).keySet()){
 			if(!indexNew.containsKey(docID)){
-				indexNew.put(docID, 0);
+				indexNew.put(docID, 0L);
 			}
-			Integer tmpVal = indexNew.get(docID);
+			Long tmpVal = indexNew.get(docID);
 			indexNew.put(docID, tmpVal + index.get(c2).get(docID));
 		}
 
@@ -367,7 +380,7 @@ public class PMICluster {
 	}
 
 
-	private void updateBatch(Integer c1, Integer c2, List<Integer> wordQueue){
+	private void updateBatch(Integer c1, Integer c2, LinkedList<Integer> wordStack){
 		// remove the clusters that were merged (and the scored pairs for them)
 		List<Integer> oldBatch = currentBatch;
 		currentBatch = new ArrayList<Integer>();
@@ -376,6 +389,8 @@ public class PMICluster {
 				currentBatch.add(c);
 			}
 		}
+
+		assert currentBatch.size() == oldBatch.size() - 2;
 
 		currentBatchScores.remove(c1);
 		currentBatchScores.remove(c2);
@@ -387,13 +402,13 @@ public class PMICluster {
 		// find what to add to the current batch
 		List<Integer> newItems = new ArrayList<Integer>();
 		newItems.add(clusterCounter);
-		if(wordQueue.size() > 0){
-			newItems.add(wordQueue.get(0));
-			wordQueue.remove(0);
+		if(wordStack.size() > 0){
+			newItems.add(wordStack.pop());
 		}
 
 		assert newItems.size() > 0;
-
+		// LOGGER.info("NEW ITEMS SIZE:" + newItems.size());
+		
 		// add to the batch and score the new cluster pairs that result
 		makePairScores(newPairs(newItems));
 
@@ -403,15 +418,15 @@ public class PMICluster {
 	}      
 
 
-	private Comparator<Entry<String, Integer> > mapComparatorByIntValue = new Comparator<Entry<String, Integer> >(){
-		public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2){
+	private Comparator<Entry<String, Long> > mapComparatorByIntValue = new Comparator<Entry<String, Long> >(){
+		public int compare(Entry<String, Long> o1, Entry<String, Long> o2){
 			return o1.getValue().compareTo(o2.getValue());
 		}
 	};
 
 
 	private void makeWordCounts(String inputPath, int maxVocabSize, int minWordCount) throws IOException{
-		Map<String, Integer> wordCountsTmp = new HashMap<String, Integer>();
+		Map<String, Long> wordCountsTmp = new HashMap<String, Long>();
 
 		Iterator<String> it = FileUtils.lineIterator(new File(inputPath), "UTF-8");
 		String doc;
@@ -424,22 +439,22 @@ public class PMICluster {
 				}
 
 				if(!wordCountsTmp.containsKey(tok)){
-					wordCountsTmp.put(tok, 0);
+					wordCountsTmp.put(tok, 0L);
 				}
 				wordCountsTmp.put(tok, wordCountsTmp.get(tok) + 1);
 			}
 		}
 
-		int tooRare = minWordCount - 1;
+		long tooRare = minWordCount - 1;
 		if(minWordCount > 1 && maxVocabSize > 0){
 			LOGGER.info("maxVocabSize and minWordCount both set.  Ignoring minWordCount.");
 		}
 
-		List<Entry<String, Integer> > wordEntries = new ArrayList<Entry<String, Integer> >();
+		List<Entry<String, Long> > wordEntries = new ArrayList<Entry<String, Long> >();
 		wordEntries.addAll(wordCountsTmp.entrySet());
 		Collections.sort(wordEntries, mapComparatorByIntValue);
 		Collections.reverse(wordEntries);
-		for(Entry<String, Integer> entry: wordEntries){
+		for(Entry<String, Long> entry: wordEntries){
 			words.add(entry.getKey());
 		}
 
